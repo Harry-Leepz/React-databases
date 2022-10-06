fb5cab5381937d93b168b134e57aa7c9b3cee3ad
Harry Dhillon <hdhillon478@gmail.com>
2022-10-06 21:10:15 +0100

    Add async / await variant of making GET requests

--- src/App.js
+++ src/App.js
@@ -6,22 +6,39 @@ import "./App.css";
 function App() {
   const [moviesData, setMoviesData] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformData = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMoviesData(transformData);
-      });
+  // USING VANILLA JS - FETCH API
+  // const fetchMoviesHandler = () => {
+  //   fetch("https://swapi.dev/api/films/")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformData = data.results.map((movie) => {
+  //         return {
+  //           id: movie.episode_id,
+  //           title: movie.title,
+  //           openingText: movie.opening_crawl,
+  //           releaseDate: movie.release_date,
+  //         };
+  //       });
+  //       setMoviesData(transformData);
+  //     });
+  // };
+
+  // ASYNC / AWAIT
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformData = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMoviesData(transformData);
   };
 
   return (
